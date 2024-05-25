56a796a273d56ffc62bbea7482890723e2f1ee14
andreaacomelli <comelliandrea21@gmail.com>
2024-05-25 18:45:19 +0200

    Inizio step 3

---
+++ src/org/gestore/eventi/Concerto.java
@@ -0,0 +1,63 @@
+package org.gestore.eventi;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Concerto extends Evento {
+	
+	private LocalTime ora;
+	private int prezzo;
+
+	public Concerto(String titolo, LocalDate data, int nPostiTotali, int nPostiPrenotati, LocalTime ora, int prezzo) {
+		super(titolo, data, nPostiTotali, nPostiPrenotati);
+		// TODO Auto-generated constructor stub
+		this.setOra(ora);
+		this.setPrezzo(prezzo);
+	}
+
+	public LocalTime getOra() {
+		return ora;
+	}
+
+	public void setOra(LocalTime ora) {
+		this.ora = ora;
+	}
+
+	public int getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(int prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	public void inserimentoDatiConcerto() {
+		DateTimeFormatter formattaOre = DateTimeFormatter.ofPattern("HH:mm");
+		
+		System.out.println("Inserisci l'orario del concerto (HH:mm):");
+		String inputOrario = scan.nextLine();
+		
+		try {
+            ora = LocalTime.parse(inputOrario, formattaOre);
+        } catch (DateTimeParseException e) {
+            System.out.println("Formato dell'orario non valido.");
+            return;
+        }
+		
+		System.out.println("Inserisci il prezzo del biglietto:");
+		int prezzo = scan.nextInt();
+		scan.nextLine(); // Consuma il resto della riga
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		DateTimeFormatter formattaOre = DateTimeFormatter.ofPattern("HH:mm");
+		DateTimeFormatter formattaData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String stringaOrario = ora.format(formattaOre);
+		
+		return getData().format(formattaData) + stringaOrario + "-" + getTitolo() + "-" + getPrezzo() + "£";
+	}
+}
--- src/org/gestore/eventi/Main.java
+++ src/org/gestore/eventi/Main.java
@@ -1,18 +1,35 @@
 package org.gestore.eventi;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Evento evento = new Evento("Evento di default", LocalDate.now().plusDays(1), 20, 0);
+		Concerto concerto = new Concerto("Evento di default", LocalDate.now().plusDays(1), 20, 0, LocalTime.now().plusHours(1), 0);
+		String scelta;
+		
+		
+		System.out.println("Che tipo di evento desideri gestire, generico o concerto?");
+		Scanner scan = new Scanner(System.in);
+		scelta = scan.nextLine().toLowerCase();
+		
+		if (scelta.equals("generico")) {
+			evento.InserimentoDati();
+		}
+		else if (scelta.equals("concerto")) {
+			concerto.InserimentoDati();
+			concerto.inserimentoDatiConcerto();
+		} else {
+			System.out.println("Inserisci un'opzione valida");
+		}
 		
 		evento.InserimentoDati();
 		
 		System.out.println("Cosa desideri fare: prenotare o disdire una prenotazione?");
-		Scanner scan = new Scanner(System.in);
-		String scelta = scan.nextLine().toLowerCase();
+		scelta = scan.nextLine().toLowerCase();
 		
 		if (scelta.equals("prenotare")) {
 			evento.prenota();
